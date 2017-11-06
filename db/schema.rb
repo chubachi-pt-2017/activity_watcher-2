bac4126eb4cf825408d87d7909d8453bdd8bc428
prius20 <a1544hm@aiit.ac.jp>
2017-11-06 21:29:20 +0900

    mod db/chema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102034955) do
+ActiveRecord::Schema.define(version: 20171104140029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "couses", force: :cascade do |t|
+    t.string   "couse_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "members", force: :cascade do |t|
     t.string   "login"
@@ -51,6 +57,7 @@ ActiveRecord::Schema.define(version: 20171102034955) do
     t.string   "student_no",     limit: 64,  default: "", null: false
     t.datetime "created_at",                              null: false
     t.datetime "updated_at",                              null: false
+    t.string   "couse_id"
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true, using: :btree
     t.index ["slack_user"], name: "index_users_on_slack_user", using: :btree
