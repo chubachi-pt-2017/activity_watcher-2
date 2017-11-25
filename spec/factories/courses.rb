ca5a4d6a2201494c4079b2dfeaeb544b6040ddd5
prius20 <a1544hm@aiit.ac.jp>
2017-11-25 21:15:25 +0900

    cre course

---
+++ spec/factories/courses.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :course do
+    title "MyString"
+    student_entry_start "2017-11-25 21:10:45"
+    student_entry_end "2017-11-25 21:10:45"
+    description "MyString"
+  end
+end
