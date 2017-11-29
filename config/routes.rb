be46f2f05a2dfddfe75fc532c555b266823fc71d
IshidaHIRO <a1601hi@aiit.ac.jp>
2017-11-30 01:15:56 +0900

    halfcording

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
-  resources :teams
+  #resources :teams
+ 
+  
   get 'auth/:provider/callback', to: 'sessions#callback'
   get 'users/logout', to: 'sessions#destroy'
   get 'users/registration', to: 'sessions#registration', as: 'user_registration'
@@ -8,9 +10,12 @@ Rails.application.routes.draw do
   
   namespace :activity_watcher, path: 'activity-watcher', as: "" do
     get '/', to: 'homes#index', as: 'activity_watcher'
+     #get "/activity-watcher/teams"
   end
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+    #root 'teams#index'
+  
 end
