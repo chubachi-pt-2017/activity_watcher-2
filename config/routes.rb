85e145e5ed844c58e0a5e5c64aa09bcce01dda0a
IshidaHIRO <a1601hi@aiit.ac.jp>
2017-12-01 21:12:58 +0900

    conflict 解消

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,7 @@ Rails.application.routes.draw do
   namespace :activity_watcher, path: 'activity-watcher', as: "" do
     get '/', to: 'homes#index', as: 'activity_watcher'
     
-    resources :teams do
-    end
+    resources :teams 
     resources :courses do
       resources :tasks
     end
