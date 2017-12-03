d9ee1c719a87e3209d59ab0f005547648a83d7b3
Kensuke <indy0214@outlook.com>
2017-12-03 22:19:46 +0900

    JS不要だったので削除

--- app/assets/javascripts/activity_watcher/application.js
+++ app/assets/javascripts/activity_watcher/application.js
@@ -14,22 +14,3 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require_tree .
-
-$(function() {
-  // 途中から固定したいボックスの情報を得る
-  var navBox = $("#js-sidebar");
-  var navOst = navBox.offset().top;
-
-  // スクロールされた際に実行
-  $(window).scroll( function() {
-     // 現在のスクロール位置と、固定したいボックスの位置を比較
-     if( $(window).scrollTop() > navOst ) {
-        // 固定用のclassを付加
-        navBox.addClass("fixBox");
-     }
-     else {
-        // 固定用のclassを削除
-        navBox.removeClass("fixBox");
-     }
-  });
-});
