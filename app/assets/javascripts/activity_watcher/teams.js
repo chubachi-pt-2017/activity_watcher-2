1f62b39f7ac31868bc577a567fbda5bfc97fc8d5
hal13 <a15z8my@aiit.ac.jp>
2017-12-14 20:06:55 +0000

    チームメンバー追加・削除時の削除ボタン表示制御を修正

--- app/assets/javascripts/activity_watcher/teams.js
+++ app/assets/javascripts/activity_watcher/teams.js
@@ -16,8 +16,8 @@ $(function() {
 });
 
 function check_content() {
-  var elements_display = $('#team_participants .nested-fields:not(:has(a.destroyed))');
-  
+  var elements_display = $('#team_participants .nested-fields:not(:has(a.destroyed)).nested-fields:has(:visible)');
+
   if(elements_display.length <= 1) {
     $('#team_participants a.add_fields').show();
     $('#team_participants a.remove_fields').hide();
