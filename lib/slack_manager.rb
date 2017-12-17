1436efaef8b5b68615597f1df514c7d94594bd56
hal13 <a15z8my@aiit.ac.jp>
2017-12-17 11:56:37 +0000

    SlackManager完成

---
+++ lib/slack_manager.rb
@@ -0,0 +1,29 @@
+require "slack"
+
+class SlackManager
+
+  def initialize(workspace_access_token)
+    @token = workspace_access_token
+  end
+  
+  # ワークスペース招待メール送信(gemでサポートされていないmethodを使うためhttpartyを使用しURL直打ち)
+  def send_invite_email(email)
+    json = HTTParty.get("#{Slack.endpoint}/users.admin.invite?token=#{@token}&email=#{email}&resend=true")
+    json['ok'] == true ? true : json['error']
+  end
+  
+  # チャンネル作成
+  def create_channel(channel_name)
+    client = Slack::Client.new token: @token
+    json = client.channels_create(name: channel_name)
+    json['ok'] == true ? true : json['error']
+  end
+  
+  # tokenの整合性チェック(登録時のvalidateで使用)
+  def check_token
+    client = Slack::Client.new token: @token
+    json = client.auth_test
+    json['ok'] == true ? json['url'] : json['error']
+  end
+
+end
