720fd741eeebf252e0b195aaa9d859c66c6ca1fa
hal13 <a15z8my@aiit.ac.jp>
2017-12-17 08:55:45 +0000

    seedの運用を「洗い替え」から「追加・更新」に変更

---
+++ db/fixtures/university.rb
@@ -0,0 +1,12 @@
+University.seed do |s|
+  s.id = 1
+  s.name = "産業技術大学院大学"
+  s.name_en = "AiiT"
+  s.email_domain = "aiit.ac.jp"
+end
+University.seed do |s|
+  s.id = 2
+  s.name = "グーグル"
+  s.name_en = "Google"
+  s.email_domain = "gmail.com"
+end
